3b95a423dcf5ea0f835ff1e36248b661049fd2d6
krokhalev <cramush@gmail.com>
2023-05-02 23:08:46 +0300

    update Two Sum

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -1,9 +1,10 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            for j in range(i + 1, len(nums)):
+                if i + 1 < len(nums):
+                    if nums[i] - target + nums[j] == 0:
+                        return [i, j]
 
 
 nums = [2, 7, 11, 15]
