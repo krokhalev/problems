7dd5512beab2b7db921454f53267deb8d8154e06
krokhalev <cramush@gmail.com>
2023-05-21 23:49:24 +0300

    update Merge Two Sorted Lists

--- 21_Merge_Two_Sorted_Lists.py
+++ 21_Merge_Two_Sorted_Lists.py
@@ -5,38 +5,34 @@ class ListNode(object):
         self.next = next
 
 
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        if list2 is None:
-            return list1
+class Solution:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        res = ListNode()
+        tail = res
 
-        while list1:
-            box = list2
-            run = True
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
 
-            while run and list1 and box:
-                if list1.val < box.val:
-                    box.next = ListNode(val=box.val, next=box.next)
-                    box.val = list1.val
-                    run = False
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
 
-                if box.next is None:
-                    box.next = ListNode(val=list1.val)
-                    run = False
-
-                box = box.next
-
-            list1 = list1.next
-
-        return list2
+        return res.next
 
 
 s = Solution()
-res = s.mergeTwoLists(
+output = s.mergeTwoLists(
     ListNode(val=2, next=ListNode(val=2, next=ListNode(val=4))),
     ListNode(val=1, next=ListNode(val=3, next=ListNode(val=4)))
 )
 
-while res:
-    print(res.val)
-    res = res.next
+while output:
+    print(output.val)
+    output = output.next
