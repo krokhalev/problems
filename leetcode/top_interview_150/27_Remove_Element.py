6f4ceb890834e76da0d5e3e29d116fb83c8e3def
krokhalev <cramush@gmail.com>
2025-03-26 20:22:25 +0300

    Add 27_Remove_Element.

---
+++ leetcode/top_interview_150/27_Remove_Element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeElement(self, nums, val):
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
+
+
+s = Solution()
+print(s.removeElement(nums = [0,1,2,2,3,0,4,2], val = 2))
